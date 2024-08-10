bbb15c0b7538b1a66aab396db73eeb4c6f76b1de
Williamsanchez-01 <williamcostasanchez@gmail.com>
2024-08-10 02:55:45 -0300

    cidades

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Itanhaém");
+Console.WriteLine("Mongaguá");
+Console.WriteLine("Guarujá");
+Console.WriteLine("Santos");
+Console.WriteLine("Peruibe");
+Console.WriteLine("São Vicente");
+
